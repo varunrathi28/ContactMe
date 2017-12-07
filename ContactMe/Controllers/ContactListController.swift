9eae3414a56b18ad6aa8b45eea098984a1766139
Varun Rathi <varunrathi28@gmail.com>
2017-12-07 18:33:33 +0530

    File structure with files added

--- ContactMe/Controllers/ContactListController.swift
+++ ContactMe/Controllers/ContactListController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContactListController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ContactMe/Helpers/Utility.swift
@@ -0,0 +1,9 @@
+//
+//  Utility.swift
+//  ContactMe
+//
+//  Created by Varun Rathi on 07/12/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import Foundation
